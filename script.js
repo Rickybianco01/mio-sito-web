7687aacbdacb2912a7bb990e795b215018041357
Rickybianco01 <133114256+Rickybianco01@users.noreply.github.com>
2023-05-17 14:02:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 window.onload = function() {
     alert("Benvenuto nel sito di EAGE - European Association Generation Excellentia!");
-}
 
-function changeColor(element) {
-    element.style.color = "#FF0000"; // Cambia il colore del testo in rosso
-}
-
-function resetColor(element) {
-    element.style.color = "#FFFFFF"; // Resetta il colore del testo a bianco
-}
-window.onload = function() {
     var image = document.getElementById('main-image');
 
     image.onmouseover = function() {
@@ -24,3 +15,12 @@ window.onload = function() {
         window.location.href = 'progetto.html'; // Sostituisci con l'URL della tua pagina di dettaglio
     }
 }
+
+function changeColor(element) {
+    element.style.color = "#FF0000"; // Cambia il colore del testo in rosso
+}
+
+function resetColor(element) {
+    element.style.color = "#FFFFFF"; // Resetta il colore del testo a bianco
+}
+
