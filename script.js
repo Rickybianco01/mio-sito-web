3eee46a3530c6b8a1846cc3ec4bcc7ff35c17fb6
Rickybianco01 <133114256+Rickybianco01@users.noreply.github.com>
2023-05-17 12:53:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ function changeColor(element) {
 function resetColor(element) {
     element.style.color = "#FFFFFF"; // Resetta il colore del testo a bianco
 }
+window.onload = function() {
+    var image = document.getElementById('main-image');
+
+    image.onmouseover = function() {
+        this.classList.add('enlarge');
+    }
+
+    image.onmouseout = function() {
+        this.classList.remove('enlarge');
+    }
+
+    image.onclick = function() {
+        window.location.href = 'progetto.html'; // Sostituisci con l'URL della tua pagina di dettaglio
+    }
+}
