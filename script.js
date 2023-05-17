f65414cd753f524e11831bcb81b0cd9f46aae62b
Rickybianco01 <133114256+Rickybianco01@users.noreply.github.com>
2023-05-17 12:42:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    alert("Benvenuto nel sito di EAGE - European Association Generation Excellentia!");
+}
+
+function changeColor(element) {
+    element.style.color = "#FF0000"; // Cambia il colore del testo in rosso
+}
+
+function resetColor(element) {
+    element.style.color = "#FFFFFF"; // Resetta il colore del testo a bianco
+}
